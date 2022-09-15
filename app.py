758a3f7e9bfa91173c5df24ee4497fcd4a1cad7b
Swetank Singh <swetanksingh@Swetanks-MacBook-Pro.local>
2022-09-15 18:40:49 +0530

    updated file#

--- app.py
+++ app.py
@@ -15,11 +15,6 @@ app.secret_key = 'jose'
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
@@ -29,6 +24,4 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
     app.run(port=5000, debug=True)

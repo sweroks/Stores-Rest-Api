e821e480e9c0a724c01a591248fd7b8537f74ccb
Swetank Singh <swetanksingh@Swetanks-MacBook-Pro.local>
2022-09-16 20:10:08 +0530

    Modified code with some app changes

--- app.py
+++ app.py
@@ -26,6 +26,13 @@ api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 
 api.add_resource(UserRegister, '/register')
-
 if __name__ == '__main__':
+    from db import db
+
+    db.init_app(app)
+
+
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
     app.run(port=5000)
